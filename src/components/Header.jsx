50560b0ea3f17a46e704f23a4292b5b5cf6dba31
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-04-21 10:25:32 +0500

    Add project on React

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+        <img src='https://cdn.pixabay.com/photo/2018/03/28/04/02/logo-3268177_960_720.png'/>
+    </header>
+
+)}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <dib>
+                <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRPrHLeU5_Ic3fTNymLhINgmM11kxPYilKF8JhLfa9IjzXBOg7c&usqp=CAU' />
+            </dib>
+            <div>
+                <img src='https://sun1-97.userapi.com/c855724/v855724522/16c61b/G6HxKnriKDM.jpg?ava=1'/>
+            </div>
+
+        </div>
+
+)}
+
+export default Profile;
